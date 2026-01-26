d0467e09f7a857f22c29c25986ca1ffbe270c19e
user <magamed773377@gmail.com>
2026-01-26 15:36:04 +0300

    add team

--- src/config/team.ts
+++ src/config/team.ts
@@ -5,7 +5,7 @@ export const TEAM_MEMBERS: TeamMember[] = [
     id: '1',
     name: 'Дикилиев Магомед',
     role: 'Co-Founder, Fullstack & Web3',
-    image: '/team/magomed.jpg', // TODO: Добавить фото Магомеда в public/team/
+    image: '/team/magomed_v2.jpg', // TODO: Добавить фото Магомеда в public/team/
   },
   {
     id: '2',
@@ -28,26 +28,34 @@ export const TEAM_MEMBERS: TeamMember[] = [
   {
     id: '5',
     name: 'Магомед Абдулаев',
-    role: 'Designer',
+    role: 'Graphic/Brand Designer',
     image: '/team/magomed_abdulaev.png',
   },
+
   {
     id: '6',
-    name: 'Ахмад Алимхаджиев',
-    role: 'Analyst',
-    image: '/team/ahmad.jpg',
-  },
-  {
-    id: '7',
     name: 'Джамбулат Солтамурадов',
     role: 'Python, FastAPI',
     image: '/team/djambulat.jpeg',
   },
   {
+    id: '7',
+    name: 'Беслан Алаудинов',
+    role: 'UI/UX Designer',
+    image: '/team/beslan.jpg',
+  },
+
+  {
     id: '8',
     name: 'Мансур Нурадиев',
     role: 'Project Manager',
     image: '/team/mansur_nuradiev.jpg',
+  },
+  {
+    id: '9',
+    name: 'Ахмад Алимхаджиев',
+    role: 'Analyst',
+    image: '/team/ahmad.jpg',
   },
   
 ];
