0fee2775340fde54042c9ffa69ade692de6dd981
user <magamed773377@gmail.com>
2026-03-21 18:33:10 +0300

    temp fixes

--- src/config/team.ts
+++ src/config/team.ts
@@ -3,9 +3,9 @@ import { TeamMember } from '../types';
 export const TEAM_MEMBERS: TeamMember[] = [
   {
     id: '1',
-    name: 'Дикилиев Магомед',
-    role: 'Co-Founder, Fullstack & Web3',
-    image: '/team/magomed_v2.jpg', // TODO: Добавить фото Магомеда в public/team/
+    name: 'Магомед Дикилиев',
+    role: 'Co-Founder, Fullstack',
+    image: '/team/magomed_v2.jpg',
   },
   {
     id: '2',
@@ -14,49 +14,61 @@ export const TEAM_MEMBERS: TeamMember[] = [
     image: '/team/ramzan.jpg',
   },
   {
-    id: '3',
-    name: 'Росул Уматгериев',
-    role: 'Backend & Mobile',
-    image: '/team/rosul.jpg',
-  },
-  {
-    id: '4',
-    name: 'Хамзат Кадиев',
-    role: 'Frontend',
-    image: '/team/khamzat.jpg',
-  },
-  {
-    id: '5',
-    name: 'Магомед Абдулаев',
-    role: 'Graphic/Brand Designer',
-    image: '/team/magomed_abdulaev.png',
-  },
-
-  {
-    id: '6',
-    name: 'Джамбулат Солтамурадов',
-    role: 'Python, FastAPI',
-    image: '/team/djambulat.jpeg',
-  },
-  {
-    id: '7',
-    name: 'Беслан Алаудинов',
-    role: 'UI/UX Designer',
-    image: '/team/beslan.jpg',
-  },
-
-  {
     id: '8',
     name: 'Мансур Нурадиев',
-    role: 'Project Manager',
+    role: 'UI/UX Designer',
     image: '/team/mansur_nuradiev.jpg',
   },
   {
-    id: '9',
-    name: 'Ахмад Алимхаджиев',
-    role: 'Analyst',
-    image: '/team/ahmad.jpg',
+    id: '6',
+    name: 'Джамбулат Солтамурадов',
+    role: 'Python, Чатботы',
+    image: '/team/djambulat.jpeg',
   },
+  // {
+  //   id: '3',
+  //   name: 'Росул Уматгериев',
+  //   role: 'Backend & Mobile',
+  //   image: '/team/rosul.jpg',
+  // },
+  // {
+  //   id: '4',
+  //   name: 'Хамзат Кадиев',
+  //   role: 'Frontend',
+  //   image: '/team/khamzat.jpg',
+  // },
+  // {
+  //   id: '5',
+  //   name: 'Магомед Абдулаев',
+  //   role: 'Graphic/Brand Designer',
+  //   image: '/team/magomed_abdulaev.png',
+  // },
+
+  // {
+  //   id: '6',
+  //   name: 'Джамбулат Солтамурадов',
+  //   role: 'Python, FastAPI',
+  //   image: '/team/djambulat.jpeg',
+  // },
+  // {
+  //   id: '7',
+  //   name: 'Беслан Алаудинов',
+  //   role: 'UI/UX Designer',
+  //   image: '/team/beslan.jpg',
+  // },
+
+  // {
+  //   id: '8',
+  //   name: 'Мансур Нурадиев',
+  //   role: 'Project Manager',
+  //   image: '/team/mansur_nuradiev.jpg',
+  // },
+  // {
+  //   id: '9',
+  //   name: 'Ахмад Алимхаджиев',
+  //   role: 'Analyst',
+  //   image: '/team/ahmad.jpg',
+  // },
   
 ];
 
